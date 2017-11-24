caa4c42d20469c995b69d03f0c2797963fc1e614
Daniel Paschal <daniel.paschal@learningfuze.com>
2017-11-24 12:19:33 -0800

    added model storage and retrieval of students as well as view rendering of students

--- sgt.js
+++ sgt.js
@@ -10,9 +10,25 @@ class StudentContentManagementSystem{
     }
     initialize(){
         this.view.initialize();
+        const students = this.model.getAllStudents();
+        this.view.renderAllStudents(students);
     }
     handleAddStudent(name, course, grade){
         console.log(`SGT CONTROLLER GOT DATA: ${name}, ${course}, ${grade}`);
+        if(typeof name !== 'string' || name.length===0){
+            this.view.displayMessage('Name must be specified');
+            return;
+        }
+        if(typeof course !== 'string' || course.length===0){
+            this.view.displayMessage('course must be specified');
+            return;
+        }
+        if(isNaN(grade) || grade <0 || grade>100){
+            this.view.displayMessage('grade must be a number and be between 0 and 100');
+            return;
+        }
+        this.model.addStudent(name, course, grade);
+        this.view.renderAllStudents( this.model.getAllStudents() );
     }
     handleCancelStudent(){
         console.log('meh');
@@ -72,11 +88,61 @@ class SCMS_view{
     handleCancelClick(){
         console.log('clear button was clicked');
     }
+    displayMessage(message){
+        $("#messageDisplay .modal-body").text(message);
+        $("#messageDisplay").modal('show');
+    }
+    renderAllStudents(students){
+        
+        this.interfaceElements.studentDisplay.empty();
+        if(students.length>0){
+            const studentElements = students.map( this.renderOneStudent );
+            this.interfaceElements.studentDisplay.append(studentElements);
+        } else {
+            this.interfaceElements.studentDisplay.append("No students available");
+        }
+    }
+    renderOneStudent( singleStudent ){
+        const row = $("<tr>");
+        const name = $("<td>", {
+            text: singleStudent.name
+        });
+        const course = $("<td>", {
+            text: singleStudent.course
+        });
+        const grade = $("<td>", {
+            text: singleStudent.grade
+        });
+        const operations = $("<td>");
+        const deleteButton = $("<button>",{
+            text: 'DELETE'
+        });
+        operations.append(deleteButton);
+        row.append(name, course, grade, operations);
+        return row;
+
+    }
 
 }
 
 class SCMS_model{
     constructor(){
+        this.studentArray = [];
+    }
+    addStudent(name, course, grade){
+        //es5
+        // var student = {
+        //     name: name,
+        //     course: course, 
+        //     grade: grade
+        // }
+        //es6 const with object structuring
+        const student = {name, course, grade};
+        this.studentArray.push(student);
+        return this.studentArray.length
+    }
+    getAllStudents(){
+        return this.studentArray;
     }
 
 }
