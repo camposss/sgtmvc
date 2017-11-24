544b613c2271f458f822093e3374c8c95226daf5
Daniel Paschal <daniel.paschal@learningfuze.com>
2017-11-24 11:50:40 -0800

    added click handler assignment and initial click handling in controller

--- sgt.js
+++ sgt.js
@@ -2,15 +2,19 @@
 class StudentContentManagementSystem{
     constructor(sgtOptions){
         this.model = new SCMS_model();
-        this.view = new SCMS_view(sgtOptions);
+        this.view = new SCMS_view(sgtOptions, this);
     }
     initialize(){
         this.view.initialize();
     }
+    handleAddStudent(name, course, grade){
+        console.log(`SGT CONTROLLER GOT DATA: ${name}, ${course}, ${grade}`);
+    }
 }
 
 class SCMS_view{
-    constructor(viewOptions){
+    constructor(viewOptions, parent){
+        this.parent = parent;
         this.interfaceElements = {};
         const defaultOptions = [
             'addButton', 'cancelButton','nameInput',
@@ -26,19 +30,40 @@ class SCMS_view{
         }
     }
     initialize(){ 
-        this.findDomElements();
+        this.attachClickHandlers();
     }
-    findDomElements(){
+    attachClickHandlers(){
+        const eventHandlers = {
+            'addButton': this.handleAddClick,
+            'cancelButton': this.handleCancelClick
+        }
         for(let key in this.interfaceElements){
             let element = $( this.interfaceElements[key] );
             if(element.length){
                 this.interfaceElements[key] = element;
+                if(eventHandlers[ key ]){
+                    element.on('click', eventHandlers[key].bind( this ));
+                    //element.on('click', (event)=> this.handleAddClick());
+                }
             } else {
                 console.error(`Missing element ${this.interfaceElements[key]}: terminating`);
                 return false;               
             }
         }
     }
+    handleAddClick(){
+        console.log('add button was clicked', this);
+        const name = this.interfaceElements.nameInput.val();
+        const course = this.interfaceElements.courseInput.val();
+        const grade = this.interfaceElements.gradeInput.val();
+        console.log(`data from click: ${name}, ${course}, ${grade}`);
+
+        this.parent.handleAddStudent(name, course, grade);
+    }
+    handleCancelClick(){
+        console.log('clear button was clicked');
+    }
+
 }
 
 class SCMS_model{
